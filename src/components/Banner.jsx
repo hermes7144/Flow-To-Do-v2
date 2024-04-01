e30d9baa0ac8fd308148a1860d7b625eca2738b3
hermes7144 <kichancho@gmail.com>
2024-04-01 15:44:13 +0900

    feat:아이콘 및 타이틀 변경

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ export default function Banner() {
     <section className='h-96 relative'>
       <div className='w-full h-full bg-cover bg-banner'></div>
       <div className='absolute w-full top-32 text-center text-gray-100'>
-        <h2 className='text-6xl'>Flow To-Do</h2>
+        <h2 className='text-6xl'>Flow ToDo</h2>
         <p className='text-2xl'>Flow 속에서 행복을 찾아보세요</p>
       </div>
     </section>
