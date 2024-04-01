fcb23264efbfa0ef274d957dfea8e10e15935fca
hermes7144 <kichancho@gmail.com>
2024-04-01 16:01:35 +0900

    feat:increase sidebar size

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const categories = [
 
 export default function Sidebar({ category, handleSheduleClick }) {
   return (
-    <div className='min-w-40 flex flex-col bg-white shadow-xl' style={{ height: 'calc(100vh - 57px)' }}>
+    <div className='min-w-60 flex flex-col bg-white shadow-xl' style={{ height: 'calc(100vh - 57px)' }}>
       {categories.map((item) => (
         <SidebarItem key={item.title} item={item} category={category} handleSheduleClick={handleSheduleClick} />
       ))}
