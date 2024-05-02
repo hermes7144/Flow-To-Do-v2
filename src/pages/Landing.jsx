1db990ccafe38f16f452a451d5c718b80a543623
hermes7144 <kichancho@gmail.com>
2024-05-02 11:09:11 +0900

    feat:landing mobile wip

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,7 +6,7 @@ export default function Landing() {
   const { login } = useAuthContext();
 
   return (
-    <div className=''>
+    <div>
       <div className='w-full bg-gray-900 flex flex-col md:flex-row justify-center items-center gap-20 p-5'>
         <div className='w-full md:w-[450px] flex flex-col justify-center items-start gap-7'>
           <h1 className='text-white text-6xl font-bold leading-[67px]'>
@@ -25,17 +25,17 @@ export default function Landing() {
       <div className='m-8 flex flex-col justify-center gap-10 md:gap-40'>
         <div className='w-full flex flex-col md:flex-row justify-center items-center gap-5 md:gap-20'>
           <img className='w-[300px] h-[200px] md:w-[550px] md:h-[400px] bg-slate-400 rounded-lg shadow-lg' src='flow-main.png' />
-          <div className=' mt-16'>
+          <div className='mt-0 md:mt-16'>
             <h2 className='text-gray-900 text-4xl font-bold tracking-tight leading-[48px]'>뽀모도로 기법을 기반으로</h2>
             <p className='w-full md:w-96 text-gray-500 text-xl leading-loose'>작업 실행 시간을 정하고, 시간이 끝날 때까지 작업에 집중하면 수확은 효율성뿐만 아니라 예상치 못한 성취감도 가져옵니다.</p>
           </div>
         </div>
         <div className='w-full flex flex-col-reverse md:flex-row justify-center items-center gap-5 md:gap-20'>
-          <div className='mt-16'>
+          <div className='mt-0 md:mt-16'>
             <h2 className='text-gray-900 text-4xl font-bold leading-[48px] tracking-tight'>무엇이든 수집하고 관리하세요</h2>
             <p className='w-full md:w-96 text-gray-500 text-xl leading-loose'>일과 공부를 위한 계획을 세우고, 쇼핑 목록을 기록하고, 생일 알림을 설정하거나 일정을 조정하는 데 사용할 수 있습니다. Flow To-Do는 인생과 직장에서 모든 일을 처리하는 데 도움이 됩니다.</p>
           </div>
-          <div className='w-[300px] md:w-[500px] h-[200px] relative'>
+          <div className='w-[300px] md:w-[500px] h-[250px] relative'>
             <img className='w-[207px] md:w-[407px] h-[158px] md:h-[268px] absolute' src='flow-kanban.png' />
             <img className='w-[160px] md:w-[320px] h-[152px] md:h-[252px] left-[145px] top-[80px] absolute opacity-80 shadow-lg' src='flow-project.png' />
           </div>
@@ -48,7 +48,7 @@ export default function Landing() {
             <img className='w-[112px] h-[155px] md:w-[212px] md:h-[255px] left-[137px] top-[25px] md:left-[237px]  absolute rounded-lg' src='flow-mobile.jpg' />
             <img className='w-[112px] h-[155px] md:w-[212px] md:h-[255px] left-0 top-[131px] md:top-[231px] absolute rounded-lg' src='flow-tablet.jpg' />
           </div>
-          <div className='mt-16'>
+          <div className='mt-0 md:mt-16'>
             <h2 className='text-gray-900 text-4xl font-bold leading-[48px] tracking-tight'>어디서나 액세스</h2>
             <p className='w-full md:w-96 text-gray-500 text-xl leading-loose'>휴대폰, 컴퓨터, 태블릿 등 언제 어디서나 작업을 기록하거나 Pomodoro를 시작할 수 있습니다.</p>
           </div>
